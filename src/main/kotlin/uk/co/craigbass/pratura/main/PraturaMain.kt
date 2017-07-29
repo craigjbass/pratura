39d69ab99f4472e82c5e9f5fc1bc0ec817dd824e
Craig J. Bass <craig@madetech.co.uk>
2017-07-29 23:37:55 +0100

    Successful HTTP server spike

---
+++ src/main/kotlin/uk/co/craigbass/pratura/main/PraturaMain.kt
@@ -0,0 +1,18 @@
+package uk.co.craigbass.pratura.main
+
+import org.jetbrains.ktor.netty.*
+import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.host.*
+import org.jetbrains.ktor.http.*
+import org.jetbrains.ktor.response.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
