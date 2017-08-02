a889b308fe4efc894c75ed4152bf4cebbfca1b72
Craig J. Bass <craig@madetech.co.uk>
2017-08-02 23:59:51 +0100

    Remove non-null check

--- src/main/kotlin/uk/co/craigbass/pratura/math/CurrencyExtensions.kt
+++ src/main/kotlin/uk/co/craigbass/pratura/math/CurrencyExtensions.kt
@@ -3,6 +3,9 @@ package uk.co.craigbass.pratura.math
 import java.math.BigDecimal
 
 fun String.toDecimal(): BigDecimal = BigDecimal(this)
+
 fun Int.toDecimal(): BigDecimal = BigDecimal(this)
-fun BigDecimal?.toCurrencyWithSymbol() = if (this == null) "£0.00" else "£${toCurrencyString()}"
-fun BigDecimal.toCurrencyString() = this.setScale(2).toPlainString()
+
+fun BigDecimal.toCurrencyWithSymbol() = "£${toCurrencyString()}"
+
+private fun BigDecimal.toCurrencyString() = this.setScale(2).toPlainString()
