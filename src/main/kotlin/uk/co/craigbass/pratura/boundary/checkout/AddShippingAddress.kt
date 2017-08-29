46b20f61a69d3f64143abc5dea9843607efb14f7
Craig J. Bass <craig@madetech.co.uk>
2017-08-29 22:07:04 +0100

    Make all input types nullable

--- src/main/kotlin/uk/co/craigbass/pratura/boundary/checkout/AddShippingAddress.kt
+++ src/main/kotlin/uk/co/craigbass/pratura/boundary/checkout/AddShippingAddress.kt
@@ -5,13 +5,13 @@ import uk.co.craigbass.pratura.boundary.checkout.AddShippingAddress.Request
 
 interface AddShippingAddress : SynchronousUseCase<Request, Unit> {
   data class Request(
-    val name: String,
+    val name: String?,
     val companyName: String?,
-    val addressLine1: String,
+    val addressLine1: String?,
     val addressLine2: String?,
     val addressLine3: String?,
-    val city: String,
-    val province: String,
-    val zipcode: String
+    val city: String?,
+    val province: String?,
+    val zipcode: String?
   )
 }
