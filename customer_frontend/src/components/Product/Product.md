a8f7642da8b0d0034a20d94e93f0f44edc442fd2
Craig J. Bass <craig@madetech.co.uk>
2017-09-30 12:47:55 +0100

    Add styleguidist

---
+++ customer_frontend/src/components/Product/Product.md
@@ -0,0 +1,5 @@
+Product example:
+
+```js
+<Product />
+```
---
+++ customer_frontend/src/components/Product/Product.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export default class Product extends React.Component {
+    render() {
+        return <span>Hello world!</span>
+    }
+}
