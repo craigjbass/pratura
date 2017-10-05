dcbf24f1ce3f700785a61e87d4447d4a85e60711
Craig J. Bass <craig@madetech.co.uk>
2017-10-05 19:01:19 +0100

    Render product catalogue

--- customer_frontend/src/components/ProductCatalogue/Product.tsx
+++ customer_frontend/src/components/ProductCatalogue/Product.tsx
@@ -16,12 +16,18 @@ export default class Product extends React.Component<Props, object> {
                     {this.props.name}
                     <small>({this.props.sku})</small>
                 </div>
-                <a href="#" onClick={this.onAddToBasket}>Add to basket</a>
+                <a
+                    href="#"
+                    data-unit-test={`add-to-basket-${this.props.sku}`}
+                    onClick={this.onAddToBasket}
+                >
+                    Add to basket
+                </a>
             </div>
         )
     }
 
-    private onAddToBasket(event: MouseEvent<HTMLAnchorElement>) {
+    private onAddToBasket = (event: MouseEvent<HTMLAnchorElement>) => {
         event.preventDefault()
         this.props.addToBasket(this.props.sku)
     }
